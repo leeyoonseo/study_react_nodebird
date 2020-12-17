d44e3f4fe04025d15d516164b4f509c3b62d2f3b
leeyoonseo <okayoon.lee@gmail.com>
2020-12-17 20:31:34 +0900

    04 [리뉴얼] React로 NodeBird SNS 만들기 - Redux-saga 연동하기
    - sata 설치하고 generator 이해하기 완료

---
+++ prepare/front/sagas/index.js
@@ -0,0 +1,10 @@
+// saga에는 generator가 나온다.
+// 문법이 generator...
+// 그래도 generator를 어느정도 이해해야한다.
+
+// Generator란?
+// 
+export default function* rootSaga(){
+
+}
+
--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -12,13 +12,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 // 리덕스 실제 구현하기
 import reducer from '../reducers';
 
-// redux-thunk
-// redux-thunk가 필요한 이유는?
-// redux-saga 쓸거고 실무에서 쓸건데....
-// login, logout 액션때.. 기능 동작 시 바로 되지 않음
-// 백엔드에 요청-응답이 필요한데...
-// (요청,실패,성공의 액션이 총 3개 필요함..)
-import thunkMiddleware from 'redux-thunk';
+// redux-saga는 따로 작성해야하는게 있음
+import rootSaga from '../sagas';
+
+// redux-saga 사용하기
+// next랑 연결하기 위해 next-redux-saga
+import createSagaMiddleware from 'next-redux-saga';
 
 // redux-thunk 커스텀 예시
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
@@ -35,9 +34,12 @@ const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
 }
 
 const configureStore = () => {
-    // redux-thunk 미들웨어 추가 예시
-    // devtool 같은 경우도 devtool 미들웨어가 장착되어져서 가능한 부분임.
-    const middlewares = [thunkMiddleware, loggerMiddleware];
+    // redux-saga 추가
+    const sagaMiddleware = createSagaMiddleware();
+    
+    // sagaMiddleware로 교체
+    // sagaMiddleware는 몇가지 기능이 있음
+    const middlewares = [sagaMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production'
         ? compose(applyMiddleware(...middlewares))
         : composeWithDevTools(applyMiddleware(...middlewares));
@@ -46,12 +48,9 @@ const configureStore = () => {
     // store는 state와 reducer를 포함하는것
     const store = createStore(reducer, enhancer);
     
-    // dispatch 방법 
-    // dispatch하는 순간 reducer로 전달이 되서 state가 변경됨
-    // store.dispatch({
-    //     type: 'CHANGE_NICKNAME',
-    //     data: 'oky',
-    // });
+    // redux-saga
+    // rootSagas는 작성해야하는게 있음
+    store.sagaTask = sagaMiddleware.runc(rootSaga);
 
     return store;
 };

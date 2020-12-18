1a5fb2b397c83ac102e6a84ff513dcfa060622d2
leeyoonseo <okayoon.lee@gmail.com>
2020-12-18 14:12:56 +0900

    04 [리뉴얼] React로 NodeBird SNS 만들기 - Redux-saga 연동하기
    - take, take시리즈, throttle 알아보기

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -16,8 +16,11 @@
 // call을 하면 리턴할때까지 기다림
 
 // 그외 다양한 이펙트들이 존재함
-// delay, debounce, throttle, akeLatest, takeLeading..
-import { all, fork, call, put, take } from 'redux-saga/effects';
+// delay, debounce, throttle, takeLatest, takeLeading..
+// 스크롤은 throttle, 단어가 완성되면 요청보내기 deboundcing?
+// 쓰로틀링 : 마지막 함수가 호출된 후 일정 시간이 지나기 전에 다시 호출되지 않도로 ㄱ하는것
+// 디바운씽: 연이어 호출되는 함수들 중 마지막 함수만 호출하도록 하는것
+import { all, fork, call, put, takeLatest, delay } from 'redux-saga/effects';
 import axios from 'axios';
 
 // 이건 제너레이터가 아님!!
@@ -35,7 +38,10 @@ function* login(action){
         // 처리해야할때는 call!!!!! (동기이기때문)
         // 데이터 여기에 2번째 인수로 전달!!
         // 인수를 여러개 넘길수도있다. 
-        const result = yield call(loginAPI, action.data);
+        // const result = yield call(loginAPI, action.data);
+
+        // 아직서버가 없어서 에러가 날수도 있으므로 가짜로 작업해보겠음..
+        yield delay(1000);
 
         // yield 붙이는 이유는 테스트 코드짜고 사용할때 유용
         // 아래와 같이 써도되는데 call 이펙트를쓰는이유?
@@ -104,19 +110,47 @@ function* addPost(action){
     }
 }
 
-// 비동기 액션 크리에이터.. 이벤트리스너 같은 역할
+// 비동기 액션 크리에이터.. 이벤트리스너 같은 역할!!
 function* watchLogin(){
     // login 액션을 실행할때까지 기다리겠다.
     // login 액션이 실행되면 login 함수를 실행한다.
-    yield take('LOG_IN_REQUEST', login);
+    yield takeLatest('LOG_IN_REQUEST', login);
 }
 
 function* watchLogout(){
-    yield take('LOG_OUT_REQUEST', logout);
+    // yield take의 치명적 단점은 1회용이다.
+    // 한번씩 실행되고나면 끝.. 
+    // 즉.. 한번 로그인 - 한번 로그아웃 시 이벤트 리스너가 사라짐
+    // 해결법은?
+    // while!! 하지만 이렇게 쓰면 직관적이지 않아서 이렇게 안하고..
+    // while(true){
+    //     yield take('LOG_OUT_REQUEST', logout);
+    // }
+
+    // takeEvery 
+    // while take는 동기, takeEvery 비동기 동작이라는 차이가 있다.
+    // yield takeEvery('LOG_OUT_REQUEST', logout);
+
+    // takeLatest 두번클릭안되게 방지하는 것
+    // 요청이 두번안가게.. 그래서 takeLatest를 많이씀
+    // 첫번째것만하고 싶으면 takeLiding이 있음
+    // takeLatest라고 무조건 마지막것만 실행하는게아니다.
+    // 동시 로딩에서..
+    // 앞에것 완료되고나면 놔두고 다음 것 실행(즉 실행이 따닥가게만 안하는듯(로딩))
+    // 치명적인 단점
+    // 프론트에서만 마지막꺼 쓴다고 생각하면됨
+    // 서버에는 2번... (요청은2번, 응답을 취소)
+    yield takeLatest('LOG_OUT_REQUEST', logout);
+
+    // takeLatest에서 2번 요청하면 처음에는 안뜨고 새로고침하면 똑같은글2개가뜸
+    // 따라서 takeLatest만으로는 안되고 throttle을 사용할 수 있다.
+    // 2초 제한.. 즉 2초안에는 1번만요청을 보낼 수 있도록 설정
+    // 특수할때쓰고 보통은 takeLatest를 위주로 씀.
+    // yield throttle('LOG_OUT_REQUEST', logout, 2000);
 }
 
 function* watchAddPost(){
-    yield take('ADD_POST_REQUEST', addPost);
+    yield takeLatest('ADD_POST_REQUEST', addPost);
 }
 
 // root saga만들고 사용하고 싶은 비동기 액션들을 하나씩 넣어준다..

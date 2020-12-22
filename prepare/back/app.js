c10e8d2b229299ea41d06025645a97a4d28de866
leeyoonseo <okayoon.lee@gmail.com>
2020-12-22 16:12:10 +0900

    05 [리뉴얼] React로 NodeBird SNS 만들기 - 백엔드 노드 서버 구축하기
    - 익스프레스로 라우팅하기 완료

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,32 +1,53 @@
 // http 서버 실행
-const http = require('http');
-// 응답을 안보내면 특정시간(30초 정도?)후에 브라우저가 자동으로
-// 응답 실패보낸다고 함.
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+const express = require('express');
 
-    // 이런식으로하면 너무 정신없어서 
-    // 코드쪼개야함
-    // if(res.method === 'GET'){
-    //     if(req.url === '/api/posts'){
+// 한번 호출해야함
+const app = express();
 
-    //     }
-    // }else if(req.method === 'POST'){
+// method, url
+// 주소창에 입력은 get 요청임
+app.get('/', (req, res) => {
+    res.send('hellow express');
+});
 
-    // }
+app.get('/api', (req, res) => {
+    res.send('hellow api');
+});
 
-    // 응답 res
-    // 요청이 뭔가 req
-    res.write('<h1>hello node</h1>');
-    res.write('<h2>hello node</h2>');
-    res.write('<h3>hello node</h3>');
-    res.write('<h4>hello node</h4>');
+app.get('/api/posts', (req, res) => {
+    // 게시글, 데이터는 보통 json임
+    // json 객체를 응답할 수 있다.
+    res.json([
+        { id: 1, content: 'hello1'},
+        { id: 2, content: 'hello2'},
+        { id: 3, content: 'hello3'},
+    ]);
+});
 
-    // 마지막에 끝낼때 1번만!!
-    res.end('hello node');
+// front에서 axios나 postman을 통해 post, delete 요청을 보낸다.
+app.post('/api/post', (req, res) => {
+    res.json({ id: 1, content: 'hello'});
+});
+
+// 협의 후 api uri 정함
+// 아무렇게나 지으면 모르니까 보통 RestAPI 방식으로 많이 짓는다.
+// !! 자주쓰는 것들
+// app.get -> 가져오다(게시글/사용자정보)
+// app.post -> 생성하다(등록 시)
+// app.put -> 전체 수정
+// app.patch -> 부분 수정
+// app.put, app.patch 수정 차이... 사용자 닉네임, 컨텐츠등은 patch 통째로 덮여씌울때 put, 즉 put사용빈도가 더 낮다
+// app.delete -> 제거
+// app.options -> 찔러보기(나 요청보낼수있니?? 서버야?)
+// app.head -> 예제에서는 안씀 (헤더만 가져와사용할때, 헤더/바디가있다.) - 헤더는 보통 바디에대한 정보.. 통신정보, 데이터 타입 정보, 용량 등등..
+// 하지만 다 합의로 결정한다. post로 메인페이지를 가져올수도있다.
+// 모르겠고 애매하면 post 쓰삼.
+// 팀원끼리 타협보삼. Rest지키기 힘들어서 타협봄..
+app.delete('/api/post', (req, res) => {
+    res.json({ id: 1 });
 });
 
 // !! back에서 작업하고나서는 서버 내리고 올려야함 node app.js! 
-server.listen(3065, () => {
+app.listen(3065, () => {
     console.log('서버 실행 중...');
 });

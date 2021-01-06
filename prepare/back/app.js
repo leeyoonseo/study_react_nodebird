7e617c2482d8b4700b857fb4980fce4917ba7ac8
leeyoonseo <okayoon.lee@gmail.com>
2021-01-06 20:54:35 +0900

    06 [리뉴얼] React로 NodeBird SNS 만들기 - Next.js 서버사이드렌더링
    - 오류문제로 강의 코드 싱크(프론트): path 추가

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');

f72ec65c5c0e06b9abcc5f292345cf14fec2769d
leeyoonseo <okayoon.lee@gmail.com>
2021-01-11 18:23:14 +0900

    07 [리뉴얼] React로 NodeBird SNS 만들기 - AWS에 배포하기
    - 프론트서버 배포하기 완료

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import postSaga from './post';
 import userSaga from './user';
 
-import {backUrl} from '../config/config';
+import { backUrl } from '../config/config';
 
 // 중복되는 주소를 baseURL로 작성하면 기본으로 axios가 적용해줌
 // AWS 백엔드 서버의 ip 넣어야함
